79a66659fabff996a3febf691d47c188d20c7f8b
Son Le <sonlexqt@gmail.com>
2016-10-30 12:22:38 -0700

    Add candidate points extraction step

--- MusicScoreRecognition/main.cpp
+++ MusicScoreRecognition/main.cpp
@@ -13,53 +13,19 @@
 using namespace std;
 using namespace cv;
 
-//Mat src;
-//Mat dst;
-//Mat src_gray;
-//Mat src_thresholded;
-//char window_name1[] = "Unprocessed Image";
-//char window_name2[] = "Processed Image";
-//
-//int main( int argc, char** argv )
-//{
-//    src = imread("test.jpg", 1);
-//    if (src.empty()) {
-//        cerr << "ERROR: Image not found." << endl;
-//        return -1;
-//    }
-//    
-//    cvtColor(src, src_gray, CV_BGR2GRAY);
-//    adaptiveThreshold(src_gray, src_thresholded, 255, CV_ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 25, 0);
-//    threshold(src_gray, src_thresholded, 127, 255, 0);
-//    
-//    //    vector<Mat> bgr_planes_original;
-//    //    split( src, bgr_planes_original);
-//    //    cout << "bgr_planes_original " << bgr_planes_original.size() << endl;
-//    //
-//    //    vector<Mat> bgr_planes_gray;
-//    //    split( src, bgr_planes_gray);
-//    //    cout << "bgr_planes_gray " << bgr_planes_gray.size() << endl;
-//    
-//    vector<Mat> bgr_planes_thresholded;
-//    split( src_thresholded, bgr_planes_thresholded);
-//    cout << "bgr_planes_thresholded " << bgr_planes_thresholded.size() << endl;
-//    
-//    namedWindow("result");
-//    imshow("result", src_thresholded);
-//    
-//    waitKey();
-//    return 0;
-//}
-
 int isDragging = 0;
 int isRoiSelected = 0;
 int REC_LINE_WIDTH = 2;
 char* WTITLE_SOURCE_IMAGE = "Source Image";
 char* WTITLE_ROI_IMAGE = "ROI Image";
 char* WTITLE_ROI_IMAGE_THRESHOLDED = "ROI Image Thresholded";
+char* WTITLE_CANDIDATE_POINTS = "Candidate Points";
 Point point1, point2; /* vertical points of the bounding box */
 Rect roiRect; /* bounding box */
-Mat img, roiImg, roiImgGray, roiImgThresholded; /* roiImg - the part of the image in the bounding box */
+Mat img,
+    roiImg,
+    roiImgGray,
+    roiImgThresholded;
 
 /*
  Helper functions
@@ -112,6 +78,21 @@ void waitForEscKey()
     }
 }
 
+bool isThisPixelRemoved(int i, int j, int value, int maxRows, int maxCols)
+{
+    if (value > 0)
+    {
+        if (i == 0 || i == 1) return true;
+        if ((int)roiImgThresholded.at<uchar>(i - 1, j) > 0)
+        {
+            if ((int)roiImgThresholded.at<uchar>(i - 2, j) > 0) return false;
+            if (j >= 1 && (int)roiImgThresholded.at<uchar>(i - 2, j - 1) > 0) return false;
+            if (j < maxCols && (int)roiImgThresholded.at<uchar>(i - 2, j + 1) > 0) return false;
+        }
+    }
+    return true;
+}
+
 /*
  Step 1 - Read source image
  */
@@ -152,6 +133,19 @@ void candidatePointsExtraction()
     cvtColor(roiImg, roiImgGray, CV_BGR2GRAY);
     threshold(roiImgGray, roiImgThresholded, 127, 255, CV_THRESH_BINARY_INV);
     imshow(WTITLE_ROI_IMAGE_THRESHOLDED, roiImgThresholded);
+    Mat blankRoiImg(roiImgThresholded.size(), CV_8U);
+    for (int i = 0; i < roiImgThresholded.rows; i++)
+        for (int j = 0; j < roiImgThresholded.cols; j++)
+        {
+            int pixelGrayScaleValue = (int) roiImgThresholded.at<uchar>(i, j);
+            if (isThisPixelRemoved(i, j, pixelGrayScaleValue, roiImgThresholded.rows, roiImgThresholded.cols))
+            {
+                // Yes: copy this pixel to blankRoiImg
+                blankRoiImg.at<uchar>(i, j) = pixelGrayScaleValue;
+            }
+        }
+    threshold(blankRoiImg, blankRoiImg, 127, 255, CV_THRESH_BINARY);
+    imshow(WTITLE_CANDIDATE_POINTS, blankRoiImg);
     waitForEscKey();
 }
 
