0f2479d2e770315a08117234b4f61ab62e62ca04
Son Le <sonlexqt@gmail.com>
2016-10-31 01:06:01 -0700

    Done: Step 4 - Rotation angle estimation

--- MusicScoreRecognition/main.cpp
+++ MusicScoreRecognition/main.cpp
@@ -20,16 +20,21 @@ char* WTITLE_SOURCE_IMAGE = "Source Image";
 char* WTITLE_ROI_IMAGE = "ROI Image";
 char* WTITLE_ROI_IMAGE_THRESHOLDED = "ROI Image Thresholded";
 char* WTITLE_CANDIDATE_POINTS = "Candidate Points";
+char* WTITLE_ROTATED_IMAGE = "Rotated image";
 Point point1, point2; /* vertical points of the bounding box */
 Rect roiRect; /* bounding box */
 Mat img,
     roiImg,
     roiImgGray,
-    roiImgThresholded, blankRoiImg;
+    roiImgThresholded,
+    blankRoiImg;
+int MAX_ROTATION_ANGLE = 30;
+int MIN_ROTATION_ANGLE = - MAX_ROTATION_ANGLE;
 
-/*
+
+/************************************
  Helper functions
- */
+ ************************************/
 void mouseDragHandler(int event, int x, int y, int flags, void* param)
 {
     if (event == CV_EVENT_LBUTTONDOWN && !isDragging)
@@ -67,17 +72,6 @@ void mouseDragHandler(int event, int x, int y, int flags, void* param)
     }
 }
 
-
-void waitForEscKey()
-{
-    int key;
-    while (1)
-    {
-        key = waitKey(10);
-        if (key == 27) break;
-    }
-}
-
 bool isThisPixelRemoved(int i, int j, int value, int maxRows, int maxCols)
 {
     if (value > 0)
@@ -93,14 +87,28 @@ bool isThisPixelRemoved(int i, int j, int value, int maxRows, int maxCols)
     return true;
 }
 
+float calculateEntropy(float PthetasAvg[], int length)
+{
+    float res = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (PthetasAvg[i] != 0) res += PthetasAvg[i] * log(PthetasAvg[i]);
+    }
+    return -res;
+}
+
+/************************************
+ Steps
+ ************************************/
+
 /*
  Step 1 - Read source image
  */
 void readSourceImage()
 {
-    img = imread("happy-birthday-rotated-white-resized.jpg", 1);
+    img = imread("test.jpg", 1);
     if (img.empty()) {
-        throw invalid_argument("ERROR: Image not found.");
+        throw invalid_argument("! ERROR: Image not found.");
         return;
     }
     imshow(WTITLE_SOURCE_IMAGE, img);
@@ -146,7 +154,7 @@ void candidatePointsExtraction()
         }
     threshold(blankRoiImg, blankRoiImg, 127, 255, CV_THRESH_BINARY);
     imshow(WTITLE_CANDIDATE_POINTS, blankRoiImg);
-    waitForEscKey();
+    waitKey(0);
 }
 
 /*
@@ -155,8 +163,61 @@ void candidatePointsExtraction()
 
 void rotationAngleEstimation()
 {
-    cout << "rotationAngleEstimation" << endl;
+    int width = blankRoiImg.cols;
+    int height = blankRoiImg.rows;
+    int entropyPsLength = MAX_ROTATION_ANGLE - MIN_ROTATION_ANGLE + 1;
+    float entropyPs[entropyPsLength];
+    Mat rotatedImg;
+    for (int a = 0; a < entropyPsLength; a++)
+    {
+        float Pthetas[height];
+        float PthetasAvg[height];
+        float sumPthetas = 0;
+        int angle = a - MAX_ROTATION_ANGLE;
+        Point2f center = Point2f(blankRoiImg.cols / 2., blankRoiImg.rows / 2.);
+        Mat rotationMatrix = cv::getRotationMatrix2D(center, angle, 1.0);
+        warpAffine(blankRoiImg, rotatedImg, rotationMatrix, blankRoiImg.size());
+        // The next calculation will use rotatedImg instead
+        for (int h = 0; h < height; h++)
+        {
+            float sumOfRows = 0;
+            for (int w = 0; w < width; w++)
+            {
+                sumOfRows += rotatedImg.at<uchar>(h, w);
+            }
+            Pthetas[h] = sumOfRows;
+            sumPthetas += Pthetas[h];
+        }
+        for (int h = 0; h < height; h++)
+        {
+            PthetasAvg[h] = Pthetas[h] / sumPthetas;
+        }
+        entropyPs[a] = calculateEntropy(PthetasAvg, height);
+    }
+    
+    int minA = 0;
+    int minEntropy = entropyPs[minA];
+    int minAngle = minA - MAX_ROTATION_ANGLE;
+    for (int a = 0; a < entropyPsLength; a++)
+    {
+        if (entropyPs[a] < minEntropy)
+        {
+            minA = a;
+            minEntropy = entropyPs[a];
+            minAngle = minA - MAX_ROTATION_ANGLE;
+        }
+    }
+    cout << "> Estimated rotation angle (deg): " << minAngle << endl;
+    Point2f center = Point2f(blankRoiImg.cols / 2., blankRoiImg.rows / 2.);
+    Mat rotationMatrix = getRotationMatrix2D(center, minAngle, 1.0);
+    warpAffine(blankRoiImg, rotatedImg, rotationMatrix, blankRoiImg.size());
+    imshow(WTITLE_ROTATED_IMAGE, rotatedImg);
+    waitKey(0);
 }
+
+/************************************
+ Main program
+ ************************************/
 int main()
 {
     readSourceImage();
